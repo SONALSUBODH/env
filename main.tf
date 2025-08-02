be611299778828a32f4fbef7e3c58ae3894fe838
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-08-02 22:06:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region = "ap-south-1"
+  access_key = ""
+  secret_key = ""
+}
+
+
 module "eks" {
  source = "../eks"
  env    =  "dev"
@@ -5,3 +12,4 @@ module "eks" {
  max_size      = "4"
  min_size      = "2"
  instance_type = ["t3.medium"]
+}
